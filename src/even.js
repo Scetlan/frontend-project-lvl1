ed18dd166e56dec9b2cd74f44995336430212e52
SVETLANA ARKHIPOVA <104462033+Scetlan@users.noreply.github.com>
2022-12-22 23:52:26 +0300

    step 9 (#4)

--- src/even.js
+++ src/even.js
@@ -8,7 +8,7 @@ console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
 const evenGames = () => {
   const question = getRandomInt(30);
-  const correctAnswer = isEven();
+  const correctAnswer = isEven(question);
 
   return { name, question, correctAnswer };
 };
