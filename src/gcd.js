5502813e486dd6961486529da8c7ff4d4681099e
Svetlana <arhipova3105@yandex.ru>
2022-12-22 17:56:31 +0300

    step 7

--- src/gcd.js
+++ src/gcd.js
@@ -6,28 +6,24 @@ const name = greetings();
 const largestDivisor = (num1, num2) => {
   let x = num1;
   let y = num2;
-  if (x === y) {
-    return 1;
-  }
-
-  while (x !== 0 && y !== 0) {
+  while (x !== y) {
     if (x > y) {
-      x %= y;
+      x -= y;
     } else {
-      y %= x;
+      y -= x;
     }
   }
-  return Math.floor(x + y);
+  return x;
 };
 
 const gcdGames = () => {
   console.log('Find the greatest common divisor of given numbers.');
 
-  const randomNum1 = getRandomInt(100);
-  const randomNum2 = getRandomInt(100);
+  const num1 = getRandomInt(100);
+  const num2 = getRandomInt(100);
 
-  const question = `${randomNum1} ${randomNum2}`;
-  const correctAnswer = largestDivisor(randomNum1, randomNum2);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = largestDivisor(num1, num2);
 
   return { name, question, correctAnswer };
 };
