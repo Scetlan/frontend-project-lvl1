8c13a9109a28fefec30c9e397d086f81cd3cdbc4
Svetlana <arhipova3105@yandex.ru>
2022-12-23 00:05:48 +0300

    fix tests

--- src/even.js
+++ src/even.js
@@ -4,7 +4,7 @@ import { getRandomInt, startGame } from './index.js';
 const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
 const name = greetings();
-console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const evenGames = () => {
   const question = getRandomInt(30);
